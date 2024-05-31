4e9beb529be62ffeea9bbc5fa55e6937fa0eaf08
Eric Zhang <ewicexclamationpoint@gmail.com>
2024-05-30 21:50:33 -0400

    Post requests are being read

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,22 @@
 import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('GET Hello World!');
 }) 
 
 app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
   res.send('POST Hello World!');
 }) 
 
 app.post('/test', (req: Request, res: Response) => {
-  console.log(req.body);
 
   res.send('POST request')
 }) 
