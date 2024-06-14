c3df45113b54be267016c43c734a90741e5c45cf
Eric Zhang <ewicexclamationpoint@gmail.com>
2024-06-14 12:03:14 -0400

    concurrently added, api endpoints connect to db now

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,36 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import 'dotenv/config';
+
+import pgPromise from 'pg-promise';
+
+const pgp = pgPromise();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const dbURL = process.env.DATABASE_URL
+
+const db = pgp(`${dbURL}/flydb`);
+
+const getUsers = async () => {
+  return await db.any('SELECT * FROM accounts');
+}
+
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('GET Hello World!');
+app.get('/', async (req: Request, res: Response) => {
+  const users = await getUsers();
+
+  console.log(users);
+
+  let out = '<h1>Users</h1><br />';
+  users.forEach(user => {
+    out += `<p>${user.name}</p>`
+  })
+
+  res.send(out);
 }) 
 
 app.post('/', (req: Request, res: Response) => {
